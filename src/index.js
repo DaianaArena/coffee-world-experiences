fd52cfb1e603d2c8fa3bd934924966581bace5d2
DaianaArena <84255114+DaianaArena@users.noreply.github.com>
2022-08-15 20:41:01 -0300

    final final tweaks

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
 import './assets/css/style.scss';
 
+const wrapper = document.querySelector(".travel");
+const cards = wrapper.querySelectorAll(".card");
+const info = wrapper.querySelectorAll(".card__info");
+const close = wrapper.querySelectorAll(".close");
+let details;
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+       
+        let cardAttribute = card.getAttribute("place");
+        info.forEach(info =>{
+            if (info.getAttribute("place") == cardAttribute){
+                info.classList.add("active");
+                details = info.querySelector(".card__info__details");
+                details.style.display = "flex";
+            } else if (info.classList.contains('active')){
+                info.classList.remove("active");
+                details = info.querySelector(".card__info__details");
+                details.style.display = "none";
+            }
+        })
+    });
+});
+
+close.forEach(close => {
+    close.addEventListener('click', () => {
+        if (close.parentElement.parentElement.classList.contains('active')){
+            close.parentElement.parentElement.classList.remove("active");
+            close.parentElement.style.display = "none";
+        }
+    });
+});
